4f6e36b90b91a152dffe3cc57c35b25f2f893e80
mrodicoss <mrodic@oss.unist.hr>
2019-05-22 10:49:52 +0200

    typo fix

--- skyscanner/settings.py
+++ skyscanner/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 RAPIDAPI_API_KEY = '5908a87257msh999fa4b31ebaa8ap143422jsn1f46950fbff4'
